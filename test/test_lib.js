e7343ed17c68269669af779728c982fe0fb208d8
Josh Vanderwillik <joshwillik@gmail.com>
2016-04-28 23:05:07 -0400

    Drip now parses tags (proper opening and closing tags)

--- test/test_lib.js
+++ test/test_lib.js
@@ -11,29 +11,81 @@ function compareTokens (actual, expected) {
   }
 }
 
-describe('Drip', function () {
-  it('should parse an empty script', function () {
-    let tokens = lex(``)
-    compareTokens(tokens, [
-      {type: 'eos'},
-    ])
-  })
-
-  it('should parse an empty script', function () {
-    let tokens = lex(`
+let tests = [
+  {
+    name: 'empty script',
+    expectedTokens: [{type: 'eos'}],
+    variations: [
+      ``,
+      `
 
 
-
-      `)
-    compareTokens(tokens, [
-      {type: 'eos'},
-    ])
-  })
-  it('should parse a doctype', function () {
-    let tokens = lex(`<!doctype html>`)
-    compareTokens(tokens, [
+      `
+    ],
+  },
+  {
+    name: 'doctype',
+    expectedTokens: [
       {type: 'doctype'},
+      {type: 'eos'}
+    ],
+    variations: [
+      `<!doctype html>`,
+      `<!doctype html>               `,
+    ],
+  },
+  {
+    name: 'basic tag',
+    expectedTokens: [
+      {type: 'tag', value: 'html'},
+      {type: 'tagEnd'},
+      {type: 'closingTag', value: 'html'},
       {type: 'eos'},
-    ])
+    ],
+    variations: [
+      `<html></html>`,
+      `<html> </html>`,
+      `<html>
+      </html>`,
+      `           <html>
+
+      </html>`,
+    ],
+  },
+  {
+    name: 'multiple tags',
+    expectedTokens: [
+      {type: 'tag', value: 'html'},
+      {type: 'tagEnd'},
+      {type: 'tag', value: 'body'},
+      {type: 'tagEnd'},
+      {type: 'closingTag', value: 'body'},
+      {type: 'closingTag', value: 'html'},
+      {type: 'eos'},
+    ],
+    variations: [
+      `<html><body></body></html>`,
+      `<html> <body> </body> </html>`,
+      `<html>
+      <body>
+      </body>
+      </html>`,
+      `           <html>
+          <body>
+</body>
+
+      </html>`,
+    ],
+  },
+]
+
+describe('Drip', function () {
+  tests.forEach(test => {
+    test.variations.forEach((variation, i) => {
+      it('should parse ' + test.name + ' #' + (i + 1), () => {
+        let tokens = lex(variation)
+        compareTokens(tokens, test.expectedTokens)
+      })
+    })
   })
 })
